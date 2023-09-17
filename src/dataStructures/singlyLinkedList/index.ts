81ed83802b683ce980439b5628ebee77639d4654
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-09-17 07:38:04 -0600

    refactor(SinglyLinkedList): use JS private getters

--- src/dataStructures/singlyLinkedList/index.ts
+++ src/dataStructures/singlyLinkedList/index.ts
@@ -3,43 +3,55 @@ type Nullable<T> = T | null
 type NullableNode<T> = Nullable<Node<T>>
 
 export class SinglyLinkedList<T> {
-  private head: NullableNode<T> = null
-  private tail: NullableNode<T> = null
-  private length = 0
+  #head: NullableNode<T> = null
+  #tail: NullableNode<T> = null
+  #length = 0
+
+  get head() {
+    return this.#head
+  }
+
+  get tail() {
+    return this.#tail
+  }
+
+  get length() {
+    return this.#length
+  }
 
   push(value: T) {
     const newNode = new Node<T>(value)
 
-    if (!this.head && !this.tail) {
-      this.head = newNode
-      this.tail = newNode
+    if (!this.#head && !this.#tail) {
+      this.#head = newNode
+      this.#tail = newNode
     } else {
-      this.tail && (this.tail.next = newNode)
-      this.tail = newNode
+      this.#tail && (this.#tail.next = newNode)
+      this.#tail = newNode
     }
 
-    this.length++
+    this.#length++
     return this
   }
 
   pop() {
-    if (!this.head) return undefined
-    if (this.head === this.tail) {
-      const popped = this.head
-      this.head = null
-      this.tail = null
+    if (!this.#head) return undefined
+    if (this.#head === this.#tail) {
+      const popped = this.#head
+      this.#head = null
+      this.#tail = null
       return popped
     }
 
-    let curr = this.head
+    let curr = this.#head
 
     while (curr.next) {
       if (!curr.next.next) {
         const popped = curr.next
         curr.next = null
 
-        this.length--
-        this.tail = curr
+        this.#length--
+        this.#tail = curr
 
         return popped
       }
@@ -50,13 +62,13 @@ export class SinglyLinkedList<T> {
   }
 
   shift() {
-    if (!this.head) return undefined
+    if (!this.#head) return undefined
 
-    const oldHead = this.head
-    this.head = this.head.next
-    this.length--
-    if (this.length === 0) {
-      this.tail = null
+    const oldHead = this.#head
+    this.#head = this.#head.next
+    this.#length--
+    if (this.#length === 0) {
+      this.#tail = null
     }
 
     return oldHead
@@ -65,15 +77,15 @@ export class SinglyLinkedList<T> {
   unshift(value: T) {
     const newNode = new Node<T>(value)
 
-    newNode.next = this.head
-    this.head = newNode
-    if (this.length === 0) this.tail = newNode
-    this.length++
+    newNode.next = this.#head
+    this.#head = newNode
+    if (this.#length === 0) this.#tail = newNode
+    this.#length++
   }
 
   get(index: number): NullableNode<T> | undefined {
-    if (index > this.length || index < 0) return undefined
-    let currentNode = this.head
+    if (index > this.#length || index < 0) return undefined
+    let currentNode = this.#head
 
     while (index > 0) {
       currentNode && (currentNode = currentNode.next)
@@ -98,7 +110,7 @@ export class SinglyLinkedList<T> {
   insert(atIndex: number, value: T) {
     if (atIndex === 0) {
       this.unshift(value)
-      this.length++
+      this.#length++
 
       return true
     }
@@ -110,7 +122,7 @@ export class SinglyLinkedList<T> {
 
       node.next = insertedNode
       insertedNode.next = nextNode
-      this.length++
+      this.#length++
 
       return true
     }
