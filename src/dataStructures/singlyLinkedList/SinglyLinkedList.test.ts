a226117a18fc62cdf2ae7553757499b68a7c2d31
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-09-17 11:28:11 -0600

    test(SinglyLinkedList): tests all current methods

---
+++ src/dataStructures/singlyLinkedList/SinglyLinkedList.test.ts
@@ -0,0 +1,119 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { SinglyLinkedList, Node } from './'
+
+describe('SinglyLinkedList', () => {
+  let linkedList: SinglyLinkedList<number>
+
+  beforeEach(() => {
+    linkedList = new SinglyLinkedList<number>()
+  })
+
+  it('should test unshift', () => {
+    const initialValue = 0
+    const expectedValue = 3
+
+    linkedList.unshift(initialValue)
+    const newListUnshiftHeadIsTail = linkedList.head === linkedList.tail
+
+    linkedList.unshift(expectedValue)
+    const unshiftListHeadValue = linkedList.head?.data
+    const unshiftListTailValue = linkedList.tail?.data
+
+    expect(newListUnshiftHeadIsTail).toBe(true)
+    expect(unshiftListHeadValue).toBe(expectedValue)
+    expect(unshiftListTailValue).toBe(initialValue)
+    expect(linkedList.length).toBe(2)
+  })
+
+  it('should test shift', () => {
+    const initialValue = 0
+    const expectedValue = 3
+
+    const emptyListShift = linkedList.shift()
+
+    linkedList.push(expectedValue)
+    linkedList.push(initialValue)
+
+    const oldHead = linkedList.head?.data
+    const shiftedReturn = linkedList.shift()?.data
+    const newHead = linkedList.head?.data
+
+    expect(emptyListShift).toBeUndefined()
+    expect(shiftedReturn).toBe(expectedValue)
+    expect(newHead).toBe(initialValue)
+    expect(oldHead).toBe(expectedValue)
+  })
+
+  it('should test set return values', () => {
+    const initialValue = 0
+    const expectedValue = 3
+
+    linkedList.push(initialValue)
+    const successfulSet = linkedList.set(0, expectedValue)
+    const failedSet = linkedList.set(1000, expectedValue)
+
+    const foundNode = linkedList.get(0)
+    const actualValue = foundNode?.data
+
+    expect(actualValue).toBe(expectedValue)
+    expect(successfulSet).toBe(true)
+    expect(failedSet).toBe(false)
+  })
+
+  it('get should return undefined when index is out of bounds', () => {
+    const expectedValue = 3
+    linkedList.push(expectedValue)
+
+    const notFoundNode = linkedList.get(1000)
+
+    expect(notFoundNode).toBeUndefined()
+  })
+
+  it('get should return the node at the provided index', () => {
+    const expectedValue = 3
+    linkedList.push(expectedValue)
+
+    const foundNode = linkedList.get(0)
+    const actualValue = foundNode?.data
+
+    expect(foundNode).toSatisfy((node) => node instanceof Node)
+    expect(actualValue).toBe(expectedValue)
+  })
+
+  it('should test pop return values', () => {
+    const initialValue = 10
+    const expectedPoppedValue = 3
+
+    linkedList.push(initialValue)
+    linkedList.push(expectedPoppedValue)
+
+    const actualPoppedvalue = linkedList.pop()?.data
+    linkedList.pop()
+    const emptListPoppedValue = linkedList.pop()
+
+    const emptyListHeadValue = linkedList.head
+    const emptyListTailValue = linkedList.tail
+
+    expect(emptyListHeadValue).toBeNull()
+    expect(emptyListTailValue).toBeNull()
+    expect(emptListPoppedValue).toBeUndefined()
+    expect(actualPoppedvalue).toBe(expectedPoppedValue)
+  })
+
+  it('should test push return values', () => {
+    const expectedValue = 3
+
+    linkedList.push(expectedValue)
+
+    const actualHeadValue = linkedList.head?.data
+    const actualTailValue = linkedList.tail?.data
+
+    expect(actualHeadValue).toBe(expectedValue)
+    expect(actualTailValue).toBe(expectedValue)
+    expect(linkedList.length).toBe(1)
+  })
+
+  it('should make a linked list', () => {
+    expect(linkedList).toSatisfy((list) => list instanceof SinglyLinkedList)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -23,30 +23,6 @@ import { isValid, data as parensData } from './algorithms/validParens'
 
 // console.log(isValid(parensData[2]))
 
-import { SinglyLinkedList } from './dataStructures/singlyLinkedList'
-
-// const list = new SinglyLinkedList<number>()
-// list.push(10)
-// list.unshift(30)
-// list.push(20)
-// list.shift()
-
-// console.log(list)
-// console.log(list.get(0))
-// console.log(list.get(-300))
-// console.log(list.get(300))
-
-// console.log(list)
-// console.log(list.set(0, 100))
-// console.log(list.set(100, 0))
-// console.log(list)
-
-// console.log(list)
-// console.log(list.insert(0, 1))
-// console.log(list.insert(-3, 1000))
-// console.log(list.insert(10, 1000))
-// console.log(list)
-
 import { shuffleArr, data as shuffleData } from './algorithms/shuffleArray'
 
 // for (const testData of shuffleData) {
