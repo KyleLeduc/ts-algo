895662ece2ef8d5c58240bc23f0a1dc312a9493a
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-06 07:53:22 -0700

    add day 3

---
+++ src/AoC2023/3/day.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+import { doWork, pt1Stub, pt2Stub } from '.'
+
+describe('aoc 2023', () => {
+  it('day 3 pt 1', () => {
+    const answer = doWork(pt1Stub)
+    console.log(answer)
+
+    expect(true).toBe(true)
+  })
+})
