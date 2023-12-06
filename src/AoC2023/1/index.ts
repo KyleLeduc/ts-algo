3a035d68aaa1db33ba8ab5d278830e7f924b4c9a
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-06 07:33:14 -0700

    Day 1
    Part 1 done
    Part 2 bugs

---
+++ src/AoC2023/1/index.ts
@@ -0,0 +1,67 @@
+import { data, pt1Stub, pt2Stub } from './stub'
+
+export const day1Data = data.split('\n')
+export const pt1StubArr = pt1Stub.split('\n')
+export const pt2StubArr = pt2Stub.split('\n')
+
+export const findCalibrationValuesPt1 = (data: string[]) => {
+  const result: number[] = []
+  const firstNumberRE = /(\d).*$/
+  const lastNumberRE = /^.*(\d)/
+
+  data.forEach((value) => {
+    const first = value.match(firstNumberRE)
+    const last = value.match(lastNumberRE)
+    if (!first || !last) return
+    result.push(parseFloat(first[1] + last[1]))
+  })
+
+  return result.reduce((prev, curr) => prev + curr, 0)
+}
+
+const wordNumberMap = [
+  undefined,
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine'
+]
+
+const wordNumRE = new RegExp(
+  `one|two|three|four|five|six|seven|eight|nine|(\\d)`,
+  'gi'
+)
+
+export const findCalibrationValuesPt2 = (data: string[]) => {
+  const result: number[] = []
+
+  data.forEach((value) => {
+    let match, lastMatch
+    const firstMatch = wordNumRE.exec(value)
+
+    while ((match = wordNumRE.exec(value)) !== null) {
+      lastMatch = match
+    }
+    if (!firstMatch) return
+    if (!lastMatch) lastMatch = firstMatch
+
+    const first = convertMatch(firstMatch[0])
+    const last = convertMatch(lastMatch[0])
+
+    result.push(parseFloat('' + first + last))
+  })
+
+  return result.reduce((prev, curr) => prev + curr, 0)
+}
+
+export const convertMatch = (match: string) => {
+  let result = parseFloat(match)
+  if (result > 9) result = parseFloat(match[0])
+
+  return !isNaN(result) ? result : wordNumberMap.indexOf(match)
+}
