6dedca353fb9438b80a723e21e29e9c736e63538
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-06 07:58:14 -0700

    add day 6

---
+++ src/AoC2023/6/stubs.ts
@@ -0,0 +1,7 @@
+export const pt1Stub = `Time:      7  15   30
+Distance:  9  40  200`
+
+export const pt2Stub = ``
+
+export const data = `Time:        48     93     84     66
+Distance:   261   1192   1019   1063`
