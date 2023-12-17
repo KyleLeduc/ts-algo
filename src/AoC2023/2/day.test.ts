2c11fc72866b84016a541f13c5fe40aeacd46b9a
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-17 12:57:01 +0200

    Day 2 pt 1 - done

--- src/AoC2023/2/day.test.ts
+++ src/AoC2023/2/day.test.ts
@@ -1,11 +1,51 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, pt1Stub, pt2Stub } from '.'
+import { doWork, processData, data, pt1Stub, pt2Stub } from '.'
+import { testCase } from './stubs'
 
-describe('aoc 2023', () => {
-  it('day 2 pt 1', () => {
-    const answer = doWork(pt1Stub)
+describe('aoc 2023 - day 2', () => {
+  it('pt 1 - Processing Data', () => {
+    const result = processData(pt1Stub)
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "1": {
+          "blue": 6,
+          "green": 2,
+          "red": 4,
+        },
+        "2": {
+          "blue": 4,
+          "green": 3,
+          "red": 1,
+        },
+        "3": {
+          "blue": 6,
+          "green": 13,
+          "red": 20,
+        },
+        "4": {
+          "blue": 15,
+          "green": 3,
+          "red": 14,
+        },
+        "5": {
+          "blue": 2,
+          "green": 3,
+          "red": 6,
+        },
+      }
+    `)
+  })
+
+  it('pt 1 - Stub Answer', () => {
+    const answer = doWork(pt1Stub, testCase)
+
+    expect(answer).toBe(8)
+  })
+
+  it('pt 1 - Answer', () => {
+    const answer = doWork(data, testCase)
+
     console.log(answer)
-
-    expect(answer).toBe(undefined)
   })
 })
--- src/AoC2023/2/index.ts
+++ src/AoC2023/2/index.ts
@@ -2,6 +2,74 @@ import { data, pt1Stub, pt2Stub } from './stubs'
 
 export { data, pt1Stub, pt2Stub }
 
-export const doWork = (data: string) => {
-  console.log(data)
+export const doWork = (data: string, testCase: GameData) => {
+  const parsedData = processData(data)
+
+  const passingGames = testGames(parsedData, testCase)
+
+  return passingGames.reduce((prev, curr) => prev + curr, 0)
+}
+
+const testGames = (games: GamesHash, testCase: GameData): number[] => {
+  const passingGames: number[] = []
+
+  Object.entries(games).forEach((game) => {
+    let isValid = true
+
+    Object.keys(testCase).forEach((testCaseKey) => {
+      const key = testCaseKey as keyof GameData
+
+      if (testCase[key] < game[1][key]) {
+        isValid = false
+        return
+      }
+    })
+
+    if (isValid) passingGames.push(parseInt(game[0]))
+  })
+
+  return passingGames
+}
+
+export const processData = (data: string) => {
+  const parsedData: GamesHash = {}
+
+  const games = data.split('\n')
+  games.forEach((game, i) => {
+    const gameNum = i + 1
+    if (!parsedData[gameNum]) parsedData[gameNum] = { ...newGameData }
+
+    const rounds = game.split(':')[1].split(';')
+
+    rounds.forEach((round) => {
+      const countColor = round.split(',')
+
+      countColor.forEach((value) => {
+        const [amountStr, colorStr] = value.trim().split(' ')
+        const amount = parseInt(amountStr)
+        const color = colorStr as keyof GameData
+
+        if (parsedData[gameNum][color] < amount)
+          parsedData[gameNum][color] = amount
+      })
+    })
+  })
+
+  return parsedData
+}
+
+const newGameData: GameData = {
+  red: 0,
+  green: 0,
+  blue: 0
+}
+
+type GamesHash = {
+  [key: number]: GameData
+}
+
+type GameData = {
+  red: number
+  green: number
+  blue: number
 }
