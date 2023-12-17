c2e44d0043042b182d81bf24fb6486d9dfa77d34
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-17 18:39:12 +0200

    Day 3 pt 1 parse data

--- src/AoC2023/3/index.ts
+++ src/AoC2023/3/index.ts
@@ -2,6 +2,41 @@ import { data, pt1Stub, pt2Stub } from './stubs'
 
 export { data, pt1Stub, pt2Stub }
 
+const symbolRE = /[%*&@=/#]/g
+const numberRE = /\d+/g
+
 export const doWork = (data: string) => {
-  console.log(data)
+  const parsedData = processData(data)
+
+  return parsedData
+}
+
+export const processData = (data: string) => {
+  const lines = data.split('\n')
+
+  return lines.map((line) => {
+    const symbolIndexes = indexSymbols(line)
+    const numberIndexes = indexNumbers(line)
+
+    const lineIndices = {
+      symbolIndexes,
+      numberIndexes,
+      input: line
+    }
+
+    return lineIndices
+  })
+}
+
+export const indexSymbols = (line: string) => {
+  const matches = [...line.matchAll(symbolRE)]
+  return matches.map((match) => match.index)
+}
+
+export const indexNumbers = (line: string) => {
+  const matches = [...line.matchAll(numberRE)]
+
+  return matches.map((match) => {
+    return { matchLength: match[0].length, startIdx: match.index }
+  })
 }
