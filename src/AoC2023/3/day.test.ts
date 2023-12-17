eef84c7d4723a30531aa1353b8dbb43ef6cc3a56
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-17 18:39:12 +0200

    Day 3 pt 1 parse data

--- src/AoC2023/3/day.test.ts
+++ src/AoC2023/3/day.test.ts
@@ -1,11 +1,48 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, pt1Stub, pt2Stub } from '.'
+import { doWork, pt1Stub, pt2Stub, indexSymbols, indexNumbers } from '.'
+import util from 'util'
 
-describe('aoc 2023', () => {
-  it('day 3 pt 1', () => {
+const testString = '..&..132..*.123'
+
+describe('aoc 2023 - Day 3', () => {
+  it('pt 1 - stub', () => {
     const answer = doWork(pt1Stub)
-    console.log(answer)
+
+    answer.forEach((line, i) => {
+      console.log(`Line: ${i}`)
+      console.log(
+        util.inspect(line, { showHidden: false, depth: null, colors: true })
+      )
+    })
 
     expect(true).toBe(true)
+  })
+
+  it('should return indices of symbols', () => {
+    const answer = indexSymbols(testString)
+
+    expect(answer).toMatchInlineSnapshot(`
+      [
+        2,
+        10,
+      ]
+    `)
+  })
+
+  it('should return indices of numbers', () => {
+    const answer = indexNumbers(testString)
+
+    expect(answer).toMatchInlineSnapshot(`
+      [
+        {
+          "matchLength": 3,
+          "startIdx": 5,
+        },
+        {
+          "matchLength": 3,
+          "startIdx": 12,
+        },
+      ]
+    `)
   })
 })
