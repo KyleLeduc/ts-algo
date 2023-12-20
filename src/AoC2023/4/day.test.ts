4f08d3a448c7ec4e82d34f96de666275a68cfd58
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-20 13:15:14 +0200

    Day 4 part 2 - stub

--- src/AoC2023/4/day.test.ts
+++ src/AoC2023/4/day.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { doWork, data, pt1Stub, pt2Stub } from '.'
+import { doWork2 } from './part2'
 
 describe('AoC 2023 - Day 4', () => {
   it('pt 1 - stub', () => {
@@ -12,5 +13,11 @@ describe('AoC 2023 - Day 4', () => {
     const answer = doWork(data)
 
     expect(answer).toBe(23673)
+  })
+
+  it('pt 2 - stub', () => {
+    const answer = doWork2(pt2Stub)
+
+    expect(answer).toBe(30)
   })
 })
