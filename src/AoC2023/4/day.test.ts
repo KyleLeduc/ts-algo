53ba2106ad6c237451d63bdca588336b64ac6244
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-20 11:30:15 +0200

    Day 4 pt 1 - answer

--- src/AoC2023/4/day.test.ts
+++ src/AoC2023/4/day.test.ts
@@ -1,11 +1,16 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, pt1Stub, pt2Stub } from '.'
+import { doWork, data, pt1Stub, pt2Stub } from '.'
 
-describe('aoc 2023', () => {
-  it.skip('day 4 pt 1', () => {
+describe('AoC 2023 - Day 4', () => {
+  it('pt 1 - stub', () => {
     const answer = doWork(pt1Stub)
-    console.log(answer)
 
-    expect(answer).toBe(undefined)
+    expect(answer).toBe(13)
+  })
+
+  it('pt 1 - answer', () => {
+    const answer = doWork(data)
+
+    expect(answer).toBe(23673)
   })
 })
--- src/AoC2023/4/index.ts
+++ src/AoC2023/4/index.ts
@@ -2,6 +2,65 @@ import { data, pt1Stub, pt2Stub } from './stubs'
 
 export { data, pt1Stub, pt2Stub }
 
+const splitDataRE = /(\d+(?: +\d+)*) +\| +(\d+(?: +\d+)+)\n?/gm
+
 export const doWork = (data: string) => {
-  console.log(data)
+  const parsedData = processData(data)
+
+  const dataHash = hashData(parsedData)
+
+  const calculatedScoreCards = calculateScore(dataHash)
+
+  const calculatedScore = calculatedScoreCards.reduce(
+    (prev, curr) => prev + curr,
+    0
+  )
+
+  return calculatedScore
+}
+
+const processData = (data: string) => {
+  const matches = Array.from(data.matchAll(splitDataRE))
+
+  const transformedData = matches.map((match) => {
+    return { winningNumbers: match[1], myNumbers: match[2] }
+  })
+
+  return transformedData
+}
+
+const hashData = (data: ReturnType<typeof processData>) => {
+  return data.map((card) => {
+    const winningNumberHash = card.winningNumbers
+      .split(' ')
+      .reduce((prev: { [key: number]: number }, curr) => {
+        const number = parseFloat(curr)
+        if (!isNaN(number)) prev[parseFloat(curr)] = 0
+
+        return prev
+      }, {})
+
+    card.myNumbers.split(' ').forEach((myNumber) => {
+      if (winningNumberHash[parseFloat(myNumber)] !== undefined)
+        winningNumberHash[parseFloat(myNumber)] += 1
+    })
+
+    return winningNumberHash
+  })
+}
+
+const calculateScore = (dataHash: ReturnType<typeof hashData>) => {
+  const cardScores = dataHash.map((card) => {
+    let score = 0
+
+    Object.values(card).forEach((number) => {
+      if (number === 1) {
+        score === 0 ? (score = 1) : (score = score * 2)
+      }
+    })
+
+    return score
+  })
+
+  return cardScores
 }
