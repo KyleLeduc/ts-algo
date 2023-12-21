8127e5ebe2d7936a6753924d0568dfe68a602e43
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-21 10:08:49 +0200

    Day 5 Initial RangeMap class isInRange and convertSourceToDestination

--- src/AoC2023/5/day.test.ts
+++ src/AoC2023/5/day.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, pt1Stub, pt2Stub } from '.'
+import { doWork, data, RangeMap, pt1Stub, pt2Stub } from '.'
 
 describe('aoc 2023', () => {
   it.skip('day 1 pt 1', () => {
@@ -7,5 +7,53 @@ describe('aoc 2023', () => {
     console.log(answer)
 
     expect(answer).toBe(undefined)
+  })
+
+  it('RangeMap - convert source to destination', () => {
+    const rangeMap = new RangeMap()
+
+    const expectedValue = 13
+    const actualValue = rangeMap.convertSourceToDestination(expectedValue)
+
+    expect(expectedValue).toBe(actualValue)
+  })
+
+  it('RangeMap.isInRange - out of range', () => {
+    const rangeMap = new RangeMap()
+
+    const testValue = 13
+    const actualValue = rangeMap.isInRange(testValue)
+
+    expect(actualValue).toBeNull()
+  })
+
+  it('RangeMap.isInRange - out of range', () => {
+    const rangeMap = new RangeMap()
+
+    const testValue = 50
+    const actualValue = rangeMap.isInRange(testValue)
+
+    expect(actualValue).toMatchInlineSnapshot(`
+      {
+        "destination": 52,
+        "range": 48,
+        "source": 50,
+      }
+    `)
+  })
+
+  it('RangeMap.isInRange - out of range', () => {
+    const rangeMap = new RangeMap()
+
+    const testValue = 98
+    const actualValue = rangeMap.isInRange(testValue)
+
+    expect(actualValue).toMatchInlineSnapshot(`
+      {
+        "destination": 50,
+        "range": 2,
+        "source": 98,
+      }
+    `)
   })
 })
--- src/AoC2023/5/index.ts
+++ src/AoC2023/5/index.ts
@@ -5,3 +5,38 @@ export { data, pt1Stub, pt2Stub }
 export const doWork = (data: string) => {
   console.log(data)
 }
+
+export class RangeMap {
+  maps = [
+    {
+      source: 98,
+      destination: 50,
+      range: 2
+    },
+    {
+      source: 50,
+      destination: 52,
+      range: 48
+    }
+  ]
+
+  isInRange(value: number) {
+    for (let i = 0; i < this.maps.length; i++) {
+      const map = this.maps[i]
+
+      if (value >= map.source && value < map.source + map.range) {
+        return map
+      }
+    }
+
+    return null
+  }
+
+  convertSourceToDestination(value: number) {
+    const map = this.isInRange(value)
+    if (map === null) return value
+    const conversion = map.destination - map.source
+
+    return value + conversion
+  }
+}
