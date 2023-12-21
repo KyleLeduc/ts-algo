06fc5489b8af74e5f161b69805576424a148cb91
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-21 15:14:03 +0200

    Day 5 pt 1 answer

--- src/AoC2023/5/day.test.ts
+++ src/AoC2023/5/day.test.ts
@@ -1,16 +1,208 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, data, RangeMap, pt1Stub, pt2Stub } from '.'
+import { doWork, data, RangeMap, pt1Stub, pt2Stub, RangeMapFactory } from '.'
+import { logFullObject } from '../../utils'
+const testRange = `50 98 2
+52 50 48`
 
-describe('aoc 2023', () => {
-  it.skip('day 1 pt 1', () => {
+describe('AoC 2023 - Day 5', () => {
+  it('pt 1 - stub', () => {
     const answer = doWork(pt1Stub)
-    console.log(answer)
 
-    expect(answer).toBe(undefined)
+    expect(answer).toBe(35)
+  })
+
+  it('pt 1 - answer', () => {
+    const answer = doWork(data)
+
+    expect(answer).toBe(111627841)
+  })
+
+  it('RangeMapFactory - findLowestLocation', () => {
+    const mapFactory = new RangeMapFactory(pt1Stub)
+
+    expect(mapFactory.findLowestLocation([79, 14, 55, 13])).toBe(35)
+  })
+
+  it('RangeMapFactory - init', () => {
+    const mapFactory = new RangeMapFactory(pt1Stub)
+
+    expect(mapFactory.rangeMaps).toMatchInlineSnapshot(`
+      [
+        RangeMap {
+          "maps": [
+            {
+              "destination": 50,
+              "range": 2,
+              "source": 98,
+            },
+            {
+              "destination": 52,
+              "range": 48,
+              "source": 50,
+            },
+          ],
+        },
+        RangeMap {
+          "maps": [
+            {
+              "destination": 0,
+              "range": 37,
+              "source": 15,
+            },
+            {
+              "destination": 37,
+              "range": 2,
+              "source": 52,
+            },
+            {
+              "destination": 39,
+              "range": 15,
+              "source": 0,
+            },
+          ],
+        },
+        RangeMap {
+          "maps": [
+            {
+              "destination": 49,
+              "range": 8,
+              "source": 53,
+            },
+            {
+              "destination": 0,
+              "range": 42,
+              "source": 11,
+            },
+            {
+              "destination": 42,
+              "range": 7,
+              "source": 0,
+            },
+            {
+              "destination": 57,
+              "range": 4,
+              "source": 7,
+            },
+          ],
+        },
+        RangeMap {
+          "maps": [
+            {
+              "destination": 88,
+              "range": 7,
+              "source": 18,
+            },
+            {
+              "destination": 18,
+              "range": 70,
+              "source": 25,
+            },
+          ],
+        },
+        RangeMap {
+          "maps": [
+            {
+              "destination": 45,
+              "range": 23,
+              "source": 77,
+            },
+            {
+              "destination": 81,
+              "range": 19,
+              "source": 45,
+            },
+            {
+              "destination": 68,
+              "range": 13,
+              "source": 64,
+            },
+          ],
+        },
+        RangeMap {
+          "maps": [
+            {
+              "destination": 0,
+              "range": 1,
+              "source": 69,
+            },
+            {
+              "destination": 1,
+              "range": 69,
+              "source": 0,
+            },
+          ],
+        },
+        RangeMap {
+          "maps": [
+            {
+              "destination": 60,
+              "range": 37,
+              "source": 56,
+            },
+            {
+              "destination": 56,
+              "range": 4,
+              "source": 93,
+            },
+          ],
+        },
+      ]
+    `)
+  })
+
+  it('RangeMapFactory - init', () => {
+    const mapFactory = new RangeMapFactory(pt1Stub)
+
+    expect(mapFactory.mapSeed(79)).toBe(82)
+    expect(mapFactory.mapSeed(14)).toBe(43)
+    expect(mapFactory.mapSeed(55)).toBe(86)
+    expect(mapFactory.mapSeed(13)).toBe(35)
+  })
+
+  it('RangeMap - init', () => {
+    const rangeMap = new RangeMap('')
+
+    const actualValue = rangeMap.init(testRange)
+
+    expect(actualValue).toMatchInlineSnapshot(`
+      [
+        {
+          "destination": 50,
+          "range": 2,
+          "source": 98,
+        },
+        {
+          "destination": 52,
+          "range": 48,
+          "source": 50,
+        },
+      ]
+    `)
+  })
+
+  it('RangeMap - constructor', () => {
+    const rangeMap = new RangeMap(testRange)
+
+    const actualValue = rangeMap.maps
+
+    expect(actualValue).toMatchInlineSnapshot(`
+      [
+        {
+          "destination": 50,
+          "range": 2,
+          "source": 98,
+        },
+        {
+          "destination": 52,
+          "range": 48,
+          "source": 50,
+        },
+      ]
+    `)
   })
 
   it('RangeMap - convert source to destination', () => {
-    const rangeMap = new RangeMap()
+    const rangeMap = new RangeMap(testRange)
 
     const expectedValue = 13
     const actualValue = rangeMap.convertSourceToDestination(expectedValue)
@@ -19,7 +211,7 @@ describe('aoc 2023', () => {
   })
 
   it('RangeMap.isInRange - out of range', () => {
-    const rangeMap = new RangeMap()
+    const rangeMap = new RangeMap(testRange)
 
     const testValue = 13
     const actualValue = rangeMap.isInRange(testValue)
@@ -27,8 +219,8 @@ describe('aoc 2023', () => {
     expect(actualValue).toBeNull()
   })
 
-  it('RangeMap.isInRange - out of range', () => {
-    const rangeMap = new RangeMap()
+  it('RangeMap.isInRange - in second range', () => {
+    const rangeMap = new RangeMap(testRange)
 
     const testValue = 50
     const actualValue = rangeMap.isInRange(testValue)
@@ -42,8 +234,8 @@ describe('aoc 2023', () => {
     `)
   })
 
-  it('RangeMap.isInRange - out of range', () => {
-    const rangeMap = new RangeMap()
+  it('RangeMap.isInRange - in first range', () => {
+    const rangeMap = new RangeMap(testRange)
 
     const testValue = 98
     const actualValue = rangeMap.isInRange(testValue)
--- src/AoC2023/5/index.ts
+++ src/AoC2023/5/index.ts
@@ -3,22 +3,69 @@ import { data, pt1Stub, pt2Stub } from './stubs'
 export { data, pt1Stub, pt2Stub }
 
 export const doWork = (data: string) => {
-  console.log(data)
+  const seedsStr = data.split('\n')[0].split(':')[1]
+  const seedArr = seedsStr
+    .trim()
+    .split(' ')
+    .map((seed) => parseInt(seed))
+  const rangeMapper = new RangeMapFactory(data)
+
+  return rangeMapper.findLowestLocation(seedArr)
+}
+
+const mapExtractionRE = /(\d+(?:\s+\d+)*(\n\d+(?:\s+\d+)*)+)/g
+
+type Map = {
+  source: number
+  destination: number
+  range: number
+}
+
+export class RangeMapFactory {
+  rangeMaps: RangeMap[]
+
+  constructor(mapsData: string) {
+    this.rangeMaps = this.init(mapsData)
+  }
+
+  init(mapsData: string) {
+    return mapsData
+      .match(mapExtractionRE)!
+      .map((rangeStr) => new RangeMap(rangeStr))
+  }
+
+  mapSeed(value: number) {
+    return this.rangeMaps.reduce(
+      (prev, curr) => curr.convertSourceToDestination(prev),
+      value
+    )
+  }
+
+  findLowestLocation(seeds: number[]) {
+    return Math.min(...seeds.map((seed) => this.mapSeed(seed)))
+  }
 }
 
 export class RangeMap {
-  maps = [
-    {
-      source: 98,
-      destination: 50,
-      range: 2
-    },
-    {
-      source: 50,
-      destination: 52,
-      range: 48
-    }
-  ]
+  maps: Map[]
+
+  constructor(mapData: string) {
+    this.maps = this.init(mapData)
+  }
+
+  init(mapData: string): Map[] {
+    const rangesHash = mapData.split('\n').map((ranges) => {
+      const [destinationStr, sourceStr, rangeStr] = ranges.split(' ')
+
+      const destination = parseFloat(destinationStr)
+      const source = parseFloat(sourceStr)
+      const range = parseFloat(rangeStr)
+
+      return { destination, source, range }
+    })
+
+    return rangesHash
+  }
 
   isInRange(value: number) {
     for (let i = 0; i < this.maps.length; i++) {
