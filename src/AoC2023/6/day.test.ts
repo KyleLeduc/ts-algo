fa9ec52145f2a7ee53ec1102cc78936652f790d1
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-22 12:12:35 +0200

    Day 6 pt2 answer

--- src/AoC2023/6/day.test.ts
+++ src/AoC2023/6/day.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, data, pt1Stub, pt2Stub } from '.'
+import { doWork, doWork2, data, pt1Stub, pt2Stub } from '.'
 
 describe('AoC 2023 - Day 6', () => {
   it('pt 1 - stub', () => {
@@ -8,9 +8,21 @@ describe('AoC 2023 - Day 6', () => {
     expect(answer).toBe(288)
   })
 
-  it('pt 1 - stub', () => {
+  it('pt 1 - answer', () => {
     const answer = doWork(data)
 
     expect(answer).toBe(1312850)
+  })
+
+  it('pt 2 - stub', () => {
+    const answer = doWork2(pt2Stub)
+
+    expect(answer).toBe(71503)
+  })
+
+  it('pt 2 - answer', () => {
+    const answer = doWork2(data)
+
+    expect(answer).toBe(36749103)
   })
 })
