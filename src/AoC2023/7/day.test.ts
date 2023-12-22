84194aaaac553dd30e91df30203092cf68802f20
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2023-12-22 16:47:42 +0200

    Day 7 pt 1 answer

--- src/AoC2023/7/day.test.ts
+++ src/AoC2023/7/day.test.ts
@@ -1,11 +1,26 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, data, pt1Stub, pt2Stub } from '.'
+import { doWork, data, pt1Stub, pt2Stub, cardStringToNumber } from '.'
+import { logFullObject } from '../../utils'
 
-describe('aoc 2023', () => {
-  it.skip('day 1 pt 1', () => {
+describe('AoC 2023 - Day 7', () => {
+  it('pt 1 - stub', () => {
     const answer = doWork(pt1Stub)
-    console.log(answer)
 
-    // expect(answer).toBe(undefined)
+    expect(answer).toBe(6440)
+  })
+
+  it('pt 1 - answer', () => {
+    const answer = doWork(data)
+
+    expect(answer).toBe(246795406)
+  })
+
+  it('card string to number', () => {
+    expect(cardStringToNumber('a')).toBe(14)
+    expect(cardStringToNumber('K')).toBe(13)
+    expect(cardStringToNumber('q')).toBe(12)
+    expect(cardStringToNumber('J')).toBe(11)
+    expect(cardStringToNumber('t')).toBe(10)
+    expect(cardStringToNumber('8')).toBe(8)
   })
 })
