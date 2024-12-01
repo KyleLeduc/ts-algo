081aa20be1e0c8ad9bfd2d0770187d8352271d17
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2024-12-01 12:09:32 -0700

    Day 1

--- src/AoC/2024/01/day.test.ts
+++ src/AoC/2024/01/day.test.ts
@@ -1,11 +1,28 @@
 import { describe, expect, it } from 'vitest'
-import { doWork, data, pt1Stub, pt2Stub } from '.'
+import { doWork, data, pt1Stub, pt2Stub } from './index'
 
 describe('aoc 2024', () => {
-  it.skip('day 1 pt 1', () => {
+  it('day 1 pt 1 test case', () => {
     const answer = doWork(pt1Stub)
-    console.log(answer)
 
-    expect(answer).toBe(undefined)
+    expect(answer).toBe(11)
+  })
+
+  it('day 1 pt 2 test case', () => {
+    const answer = doWork(pt2Stub, false)
+
+    expect(answer).toBe(31)
+  })
+
+  it('day 1 pt 1', () => {
+    const answer = doWork(data)
+
+    expect(answer).toBe(2815556)
+  })
+
+  it('day 1 pt 2', () => {
+    const answer = doWork(data, false)
+
+    expect(answer).toBe(23927637)
   })
 })
--- src/AoC/2024/01/index.ts
+++ src/AoC/2024/01/index.ts
@@ -1,5 +1,67 @@
 export { data, pt1Stub, pt2Stub } from './stubs'
 
-export const doWork = (data: string) => {
-  console.log(data)
+export const doWork = (data: string, pt1 = true) => {
+  const processedData = processData(data)
+
+  if (pt1) {
+    return partOne(processedData)
+  } else {
+    return partTwo(processedData)
+  }
+}
+
+/**
+ * Convert the string data into 2 sorted arrays of numbers
+ *
+ * @param data the string data to process
+ * @returns 2 sorted arrays of numbers separated into columns
+ */
+const processData = (data: string) => {
+  const re = new RegExp(/(\d+)\s+(\d+)/g)
+  const firstNumbers: number[] = []
+  const secondNumbers: number[] = []
+  let match
+
+  // console.log('match', re.exec('100  200'))
+
+  while ((match = re.exec(data)) !== null) {
+    firstNumbers.push(Number(match[1]))
+    secondNumbers.push(Number(match[2]))
+  }
+
+  firstNumbers.sort((a, b) => a - b)
+  secondNumbers.sort((a, b) => a - b)
+
+  return { firstNumbers, secondNumbers }
+}
+
+const partOne = ({
+  firstNumbers,
+  secondNumbers
+}: ReturnType<typeof processData>) => {
+  let separation = 0
+
+  for (const [index, firstLocationId] of firstNumbers.entries()) {
+    const secondLocationId = secondNumbers[index]
+
+    separation += Math.abs(firstLocationId - secondLocationId)
+  }
+
+  return separation
+}
+
+const partTwo = ({
+  firstNumbers,
+  secondNumbers
+}: ReturnType<typeof processData>) => {
+  let similarityScore = 0
+
+  // for each number in first arr, find count of matches in second arr
+  for (const number of firstNumbers) {
+    const count = secondNumbers.filter((n) => n === number).length
+
+    similarityScore += count * number
+  }
+
+  return similarityScore
 }
