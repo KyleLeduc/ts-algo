af93865455e3776802a15581412fd2e82da32372
Kyle Leduc <72325360+KyleLeduc@users.noreply.github.com>
2024-12-01 12:14:08 -0700

    feat: add part one & part two controller to template

--- src/AoC/2024/template/index.ts
+++ src/AoC/2024/template/index.ts
@@ -1,5 +1,19 @@
 export { data, pt1Stub, pt2Stub } from './stubs'
 
-export const doWork = (data: string) => {
+export const doWork = (data: string, dayOne = true) => {
   console.log(data)
+
+  if (dayOne) {
+    return partOne(data)
+  } else {
+    return partTwo(data)
+  }
+}
+
+const partOne = (data: string) => {
+  return 0
+}
+
+const partTwo = (data: string) => {
+  return 0
 }
